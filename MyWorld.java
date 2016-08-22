3629b111e4866f7f8e178a7c13a24becc2c20da4
tidjungs <waewprach.s@ku.th>
2016-08-22 13:56:37 +0700

    first commit + add object map

---
+++ MyWorld.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Object object = new Object();
+        addObject(object,590,358);
+    }
+    public void act()
+    {
+        createObject();
+    }
+    public void createObject()
+    {
+        if(Greenfoot.getRandomNumber(1000) < 1) {
+            Object object = new Object();
+            addObject(object,590,358);
+        }
+    }
+}
---
+++ Object.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Object here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Object extends Actor
+{
+    /**
+     * Act - do whatever the Object wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int movement = -5;
+    public void act() 
+    {
+        move(movement);
+        clearObject();
+    }
+    public void clearObject()
+    {
+        if(getX() == 0) {
+            movement = 5;
+        }
+    }
+}
